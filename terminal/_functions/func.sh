1f0292d5996437d98e032e63d9eba29e3a96cd73
Rickard Laurin <rickard.laurin@gmail.com>
2020-09-04 13:05:56 +0200

    feat: use npm in run script

--- terminal/_functions/func.sh
+++ terminal/_functions/func.sh
@@ -33,8 +33,8 @@ run() {
 
 			if [[ -n "$selected_script" ]]; then
 					script_name=$(echo "$selected_script" | awk -F ': ' '{gsub(/"/, "", $1); print $1}' | awk '{$1=$1};1')
-					print -s "yarn run "$script_name;
-					yarn run $script_name;
+					print -s "npm run "$script_name;
+					npm run $script_name;
 			else
 					echo "Exit: You haven't selected any script"
 			fi
