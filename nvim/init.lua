3cf04d0b440628fcb8e504920a4ff8cdf895aa26
Rickard Natt och Dag <rdag@invidi.com>
2023-01-03 15:39:48 +0100

    fix(nvim-tree): show on right side and close when opening file

--- nvim/init.lua
+++ nvim/init.lua
@@ -98,6 +98,14 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup{
+  actions = {
+    open_file = {
+      quit_on_open = true
+    }
+  },
+  view = {
+    side = 'right',
+  },
   renderer = {
     icons = {
       show = {
