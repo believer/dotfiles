72b7ccb929445106abfd2038ef8747b0f0defce4
Rickard Natt och Dag <rdag@invidi.com>
2023-01-05 13:56:57 +0100

    feat(harpoon): update mappings

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>af", mark.add_file)
-vim.keymap.set("n", "<leader>rf", mark.rm_file)
-vim.keymap.set("n", "<leader>cf", mark.clear_all)
+vim.keymap.set("n", "<leader>ha", mark.add_file)
+vim.keymap.set("n", "<leader>hr", mark.rm_file)
+vim.keymap.set("n", "<leader>hc", mark.clear_all)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
