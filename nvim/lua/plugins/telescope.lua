cc1eb712224b41b74c8b3ccb00d974c61944dda5
Rickard Natt och Dag <rdag@invidi.com>
2023-01-05 10:48:14 +0100

    chore: remove folding for now, it's annoying

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,10 +9,10 @@ map("n", "<leader>sd", telescope.lsp_document_symbols)
 map("n", "<leader>sw", telescope.lsp_dynamic_workspace_symbols)
 
 -- Workaround for treesitter folds when using Packer
-vim.api.nvim_create_autocmd({ "BufEnter", "BufAdd", "BufNew", "BufNewFile", "BufWinEnter" }, {
-	group = vim.api.nvim_create_augroup("TS_FOLD_WORKAROUND", {}),
-	callback = function()
-		vim.opt.foldmethod = "expr"
-		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufAdd", "BufNew", "BufNewFile", "BufWinEnter" }, {
+-- 	group = vim.api.nvim_create_augroup("TS_FOLD_WORKAROUND", {}),
+-- 	callback = function()
+-- 		vim.opt.foldmethod = "expr"
+-- 		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- 	end,
+-- })
