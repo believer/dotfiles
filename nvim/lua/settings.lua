e3f5bdf6a3af1bce4989da42ec1d529dccb7cd93
Rickard Natt och Dag <rdag@invidi.com>
2023-01-05 08:49:05 +0100

    feat: enable undofile to preserve undo history

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -40,3 +40,6 @@ map("n", "<C-J>", "<C-W><C-J>")
 map("n", "<C-K>", "<C-W><C-K>")
 map("n", "<C-L>", "<C-W><C-L>")
 map("n", "<C-H>", "<C-W><C-H>")
+
+-- Persist undos after buffers are unloaded
+o.undofile = true
