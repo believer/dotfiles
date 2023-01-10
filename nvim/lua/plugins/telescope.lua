404896a60bbe3f64890ad69d246eb6b234b04fae
Rickard Natt och Dag <rdag@invidi.com>
2023-01-10 09:48:43 +0100

    fix(telescope): close on esc instead of switching to normal mode

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,6 +2,17 @@ local map = require("utils").map
 local telescope = require("telescope.builtin")
 
 map("n", "<leader>;", telescope.find_files)
+-- Setup
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				-- Close telescope with <Esc> instead of switching to normal mode
+				["<esc>"] = actions.close,
+			},
+		},
+	},
+})
 map("n", "??", telescope.live_grep)
 map("n", "<leader>o", telescope.buffers)
 map("n", "<leader>fh", telescope.help_tags)
