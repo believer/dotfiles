b4a00e08ca650dd2fdb5a8402978db6d486e73a3
Rickard Natt och Dag <rdag@invidi.com>
2023-01-13 13:20:31 +0100

    chore: add util for creating autocommands

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,3 +1,4 @@
+local api = vim.api
 local M = {}
 
 function M.map(mode, from, to, opts)
@@ -8,6 +9,21 @@ function M.map(mode, from, to, opts)
 	end
 
 	vim.keymap.set(mode, from, to, options)
+end
+
+function M.add_autocommands(definitions)
+	for group_name, definition in pairs(definitions) do
+		local triggers = definition.triggers
+		local callback = definition.callback
+		local pattern = definition.pattern or "*"
+		local group_opts = definition.group_opts or {}
+
+		api.nvim_create_autocmd(triggers, {
+			group = vim.api.nvim_create_augroup(group_name, group_opts),
+			pattern = pattern,
+			callback = callback,
+		})
+	end
 end
 
 return M
