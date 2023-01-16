803a7ea040bdaa70b524618ca704c395e7e9951b
Rickard Natt och Dag <rdag@invidi.com>
2023-01-16 13:18:16 +0100

    feat(lazy): hide notifications on updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,39 +1,42 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("settings")
 require("lazy").setup("plugins", {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	performance = {
-		cache = {
-			enabled = true,
-		},
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"matchit",
-				"matchparen",
-				"netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+  change_detection = {
+    notify = false,
+  },
 })
 require("snippets")
 
@@ -48,13 +51,13 @@ U.map("n", ":Gss", vim.cmd.Git)
 -- Highlight on yank
 -- Taken from kickstart.nvim
 local autocommands = {
-	highlight_yank = {
-		group_opts = { clear = true },
-		triggers = { "TextYankPost" },
-		callback = function()
-			vim.highlight.on_yank()
-		end,
-	},
+  highlight_yank = {
+    group_opts = { clear = true },
+    triggers = { "TextYankPost" },
+    callback = function()
+      vim.highlight.on_yank()
+    end,
+  },
 }
 
 U.add_autocommands(autocommands)
