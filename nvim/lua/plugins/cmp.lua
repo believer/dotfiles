1f6e7b855d7fcdff4d96b5591d9d82f50c8d7981
Rickard Natt och Dag <rdag@invidi.com>
2023-01-17 14:02:30 +0100

    chore(cmp): remove copilot source

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,63 +1,62 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
-		"saadparwaiz1/cmp_luasnip",
-		-- Snippets
-		"L3MON4D3/LuaSnip",
-	},
-	event = "InsertEnter",
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    "saadparwaiz1/cmp_luasnip",
+    -- Snippets
+    "L3MON4D3/LuaSnip",
+  },
+  event = "InsertEnter",
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
-		cmp.setup({
-			-- Snippet expansions from Luasnip
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
+    cmp.setup({
+      -- Snippet expansions from Luasnip
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
 
-			-- Key mappings to navigate completion menu
-			mapping = cmp.mapping.preset.insert({
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
+      -- Key mappings to navigate completion menu
+      mapping = cmp.mapping.preset.insert({
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
 
-			-- Add additional completion sources
-			sources = cmp.config.sources({
-				{ name = "copilot", group_index = 2 }, -- Copilot
-				{ name = "nvim_lsp" }, -- LSP
-				{ name = "luasnip" }, -- Snippets
-				{ name = "buffer" }, -- Texts from buffer
-				{ name = "path" }, -- File paths
-			}),
-		})
-	end,
+      -- Add additional completion sources
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" }, -- LSP
+        { name = "luasnip" }, -- Snippets
+        { name = "buffer" }, -- Texts from buffer
+        { name = "path" }, -- File paths
+      }),
+    })
+  end,
 }
