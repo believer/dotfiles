9715481dbef493f3b0d01799767b79b92a19dd66
Rickard Natt och Dag <rdag@invidi.com>
2023-01-19 13:49:54 +0100

    fix(format): test async formatting on save

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -85,30 +85,50 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     event = "VeryLazy",
+    dependencies = {
+      "lukas-reineke/lsp-format.nvim",
+    },
     config = function()
       local null_ls = require("null-ls")
       local b = null_ls.builtins
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      require("lsp-format").setup({})
+
+      -- local lsp_formatting = function(bufnr)
+      --   vim.lsp.buf.format({
+      --     filter = function(client)
+      --       -- apply whatever logic you want (in this example, we'll only use null-ls)
+      --       return client.name == "null-ls"
+      --     end,
+      --     bufnr = bufnr,
+      --   })
+      -- end
+
+      -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+      -- -- add to your shared on_attach callback
+      -- local on_attach = function(client, bufnr)
+      --   if client.supports_method("textDocument/formatting") then
+      --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      --     vim.api.nvim_create_autocmd("BufWritePre", {
+      --       group = augroup,
+      --       buffer = bufnr,
+      --       callback = function()
+      --         lsp_formatting(bufnr)
+      --       end,
+      --     })
+      --   end
+      -- end
 
       null_ls.setup({
         -- Format on save
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
-              end,
-            })
-          end
-        end,
+        on_attach = require("lsp-format").on_attach,
 
         sources = {
           b.completion.spell,
           b.diagnostics.stylelint,
+          b.diagnostics.eslint_d,
           b.formatting.prettier,
+          b.formatting.eslint_d,
           b.formatting.stylua,
           b.formatting.rescript,
           b.formatting.rustfmt,
