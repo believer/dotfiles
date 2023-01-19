92f36dadf17f284c7558f598ce95202d245956c8
Rickard Natt och Dag <rdag@invidi.com>
2023-01-19 19:01:17 +0100

    fix(lsp): use sync formatting
    It doesn't seem to work great with async and things like
    vitest that watches for file changes

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -91,44 +91,41 @@ return {
     config = function()
       local null_ls = require("null-ls")
       local b = null_ls.builtins
-      require("lsp-format").setup({})
 
-      -- local lsp_formatting = function(bufnr)
-      --   vim.lsp.buf.format({
-      --     filter = function(client)
-      --       -- apply whatever logic you want (in this example, we'll only use null-ls)
-      --       return client.name == "null-ls"
-      --     end,
-      --     bufnr = bufnr,
-      --   })
-      -- end
+      local lsp_formatting = function(bufnr)
+        vim.lsp.buf.format({
+          filter = function(client)
+            -- apply whatever logic you want (in this example, we'll only use null-ls)
+            return client.name == "null-ls"
+          end,
+          bufnr = bufnr,
+        })
+      end
 
-      -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-      -- -- add to your shared on_attach callback
-      -- local on_attach = function(client, bufnr)
-      --   if client.supports_method("textDocument/formatting") then
-      --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      --     vim.api.nvim_create_autocmd("BufWritePre", {
-      --       group = augroup,
-      --       buffer = bufnr,
-      --       callback = function()
-      --         lsp_formatting(bufnr)
-      --       end,
-      --     })
-      --   end
-      -- end
+      -- add to your shared on_attach callback
+      local on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              lsp_formatting(bufnr)
+            end,
+          })
+        end
+      end
 
       null_ls.setup({
         -- Format on save
-        on_attach = require("lsp-format").on_attach,
+        on_attach = on_attach,
 
         sources = {
           b.completion.spell,
           b.diagnostics.stylelint,
-          b.diagnostics.eslint_d,
           b.formatting.prettier,
-          b.formatting.eslint_d,
           b.formatting.stylua,
           b.formatting.rescript,
           b.formatting.rustfmt,
