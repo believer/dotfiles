8bcc4e99e498af7012d7ed840dabb94dd249bb26
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-02-16 19:55:49 +0100

    feat(snippets): add rescript snippets

--- nvim/lua/snippets/rescript.lua
+++ nvim/lua/snippets/rescript.lua
@@ -4,6 +4,38 @@ local regular_snippets = {
 		name = "Js.log",
 		dscr = "Console log",
 	}, fmt("Js.log({})", { i(0, "data") })),
+
+	s(
+		{
+			trig = "swc",
+			name = "Switch case",
+			dscr = "Adds a switch case statement",
+		},
+		fmt(
+			[[
+switch {} {{
+
+}}
+]],
+			{ i(0, "data") }
+		)
+	),
+
+	s(
+		{
+			trig = "swcr",
+			name = "Switch case wrapped",
+			dscr = "Adds a switch case statement for use in JSX",
+		},
+		fmt(
+			[[
+{{switch {} {{
+
+}}}}
+]],
+			{ i(0, "data") }
+		)
+	),
 }
 
 return regular_snippets
