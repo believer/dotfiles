186e8aaf66857f57eecccb8ae255d45913fc2c99
Rickard Natt och Dag <rdag@invidi.com>
2023-02-17 08:11:15 +0100

    feat(snippets): add vue

--- nvim/lua/snippets/vue.lua
+++ nvim/lua/snippets/vue.lua
@@ -1,1 +1,22 @@
 require("luasnip").filetype_extend("vue", { "typescript" })
+
+local regular_snippets = {
+	s(
+		{
+			trig = "vue",
+			name = "Vue component",
+			dscr = "A simple Vue SFC using TypeScript and script setup",
+		},
+		fmt(
+			[[<template>
+</template>
+
+<script lang="ts" setup>
+{}
+</script>]],
+			{ i(0) }
+		)
+	),
+}
+
+return regular_snippets
