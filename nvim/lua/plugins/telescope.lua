82de78707e306e60560517a05bc7ed9e29afe2c7
Rickard Natt och Dag <rdag@invidi.com>
2023-02-23 15:01:09 +0100

    fix(telescope): display dotfiles

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -14,6 +14,11 @@ return {
 
 			-- Setup
 			require("telescope").setup({
+				pickers = {
+					find_files = {
+						find_command = { "rg", "--files", "--hidden", "--glob", "!.git" },
+					},
+				},
 				defaults = {
 					mappings = {
 						i = {
