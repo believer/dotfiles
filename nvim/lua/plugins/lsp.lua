9bc049d05adf8e81984bda8ceddfbd5f8b17a218
Rickard Natt och Dag <rdag@invidi.com>
2023-03-06 14:57:38 +0100

    feat(lsp): add border to hover windows

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -75,6 +75,22 @@ return {
 				})
 			end
 
+			-- Display a border around the hover window for better readability.
+			local _border = "single"
+
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+				border = _border,
+			})
+
+			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+				border = _border,
+			})
+
+			vim.diagnostic.config({
+				float = { border = _border },
+			})
+
+			-- Setup the LSPs.
 			lspconfig.volar.setup({
 				on_attach = on_attach,
 				capabilities = capabilities,
