2bb7abdfca074d2d930b7da2618863a7d2946999
Rickard Natt och Dag <rdag@invidi.com>
2023-04-03 14:50:13 +0200

    feat: update setup script

--- setup.sh
+++ setup.sh
@@ -1,22 +1,31 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo "Installing Homebrew packages..."
+
 brew install antibody
-brew install git
+brew install bat
 brew install fnm
+brew install git
 brew install gnupg # gpg
 brew install kitty
-brew install fnm
 brew install neovim
 brew install opendevtools/supreme/supreme
+brew install opendevtools/todos/todos
 brew install ripgrep
 brew install the_silver_searcher
 brew install tmux
 brew install tmuxinator
 
+# Node LTS
+echo "Installing Node..."
+fnm install 18
+
 # Rust
+echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install cargo-watch
 
 # Add yarn (> Node 16.10)
+echo "Setup Yarn..."
 corepack enable
