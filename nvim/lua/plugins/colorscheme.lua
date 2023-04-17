d39c9f1d0334d396772d6a2ec458fdca50fbab8d
Rickard Natt och Dag <rdag@invidi.com>
2023-04-17 09:43:12 +0200

    refactor: update colorscheme command

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,6 @@ return {
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		vim.cmd("colorscheme tokyonight-night")
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
