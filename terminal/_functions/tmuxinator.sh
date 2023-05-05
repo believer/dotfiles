6a9fc927db44a30970d748cd9e03c449f01de054
Rickard Natt och Dag <rdag@invidi.com>
2023-05-05 13:16:10 +0200

    chore: remove tmuxinator function

--- terminal/_functions/tmuxinator.sh
+++ terminal/_functions/tmuxinator.sh
@@ -2,25 +2,4 @@
 
 export TMUXINATOR_CONFIG="$HOME/.dotfiles/tmuxinator"
 
-_tmuxinator() {
-  local commands projects
-  commands=(${(f)"$(tmuxinator commands zsh)"})
-  projects=(${(f)"$(tmuxinator completions start)"})
-
-  if (( CURRENT == 2 )); then
-    _alternative \
-      'commands:: _describe -t commands "tmuxinator subcommands" commands' \
-      'projects:: _describe -t projects "tmuxinator projects" projects'
-  elif (( CURRENT == 3)); then
-    case $words[2] in
-      copy|cp|c|debug|delete|rm|open|o|start|s|edit|e)
-        _arguments '*:projects:($projects)'
-      ;;
-    esac
-  fi
-
-  return
-}
-
-compdef _tmuxinator tmuxinator mux
 alias mux="tmuxinator"
