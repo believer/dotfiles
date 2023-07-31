80a767a2ea0a981774adecd2c1b844ff13f68050
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-07-31 13:54:24 +0200

    feat: add gpg setup using keychain

--- setup.sh
+++ setup.sh
@@ -11,7 +11,6 @@ brew install fnm
 brew install fzf
 brew install gh
 brew install hub
-brew install gnupg # gpg
 brew install neovim
 brew install opendevtools/supreme/supreme
 brew install opendevtools/todos/todos
@@ -19,6 +18,15 @@ brew install ripgrep
 brew install the_silver_searcher
 brew install tmux
 brew install tmuxinator
+
+echo "Setting up GPG"
+brew install gnupg # gpg
+brew install keychain # add ability to save in keychain
+
+# Set pinentry to use macos keychain
+mkdir -m 0700 ~/.gnupg
+echo "pinentry-program $(brew --prefix)/bin/pinentry-mac" | tee ~/.gnupg/gpg-agent.conf
+pkill -TERM gpg-agent
 
 # Node LTS
 echo "Installing Node..."
