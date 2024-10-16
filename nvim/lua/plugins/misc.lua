061812dc276c3f7b9707b2c50c90781cd9ad6a87
Rickard Natt och Dag <rickard@nattochdag.dev>
2024-10-16 20:41:16 +0200

    chore: remove unused flash commands

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -58,9 +58,7 @@ return {
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
 	},
 }
