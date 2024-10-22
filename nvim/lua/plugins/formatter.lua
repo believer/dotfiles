13641755d02fd67e3bb49575096fe3897ce9f5ca
Rickard Natt och Dag <rickard.natt-och-dag@seb.se>
2024-10-22 09:05:38 +0200

    feat: use conform for formatting

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,62 +1,34 @@
 return {
 	{
-		"mhartington/formatter.nvim",
-		event = "VeryLazy",
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		cmd = { "ConformInfo" },
 		config = function()
-			-- Format on save
-			vim.api.nvim_create_autocmd("BufWritePost", { command = "FormatWriteLock" })
-
 			local formatter_settings = {
-				filetype = {
-					go = {
-						require("formatter.filetypes.go").gofmt,
-					},
-					html = {
-						require("formatter.filetypes.html").prettierd,
-					},
-					lua = {
-						require("formatter.filetypes.lua").stylua,
-					},
-					css = {
-						require("formatter.filetypes.css").prettierd,
-					},
-					json = {
-						require("formatter.filetypes.json").prettierd,
-					},
-					markdown = {
-						require("formatter.filetypes.markdown").prettierd,
-					},
-					rust = {
-						require("formatter.filetypes.rust").rustfmt,
-					},
-					sql = {
-						require("formatter.filetypes.sql").pgformat,
-					},
-					rescript = {
-						function()
-							vim.lsp.buf.format()
-						end,
-					},
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+				formatters_by_ft = {
+					css = { "prettierd" },
+					go = { "gofmt" },
+					json = { "prettierd" },
+					markdown = { "prettierd" },
+					rust = { "rustfmt" },
+					lua = { "stylua" },
 				},
 			}
 
-			-- Add same settings for all javascript filetypes
+			-- Add same options for all JS/TS types
 			local js_types = { "javascript", "javascriptreact", "typescript", "typescriptreact" }
 
 			for _, type in ipairs(js_types) do
-				formatter_settings.filetype[type] = {
-					require("formatter.filetypes.typescript").biome,
+				formatter_settings.formatters_by_ft[type] = {
+					"biome-check",
 				}
 			end
 
-			require("formatter").setup(formatter_settings)
-
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = { "*.templ" },
-				callback = function()
-					vim.lsp.buf.format()
-				end,
-			})
+			require("conform").setup(formatter_settings)
 		end,
 	},
 }
