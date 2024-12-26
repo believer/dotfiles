7244fc58f6627eb57374fbc252abdc786ad913a0
Rickard Natt och Dag <rickard@nattochdag.dev>
2024-12-26 19:17:18 +0100

    chore: update blink.cmp settings

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,19 +1,23 @@
 return {
 	{
 		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
 		dependencies = { "rafamadriz/friendly-snippets", "L3MON4D3/LuaSnip" },
-
-		-- use a release tag to download pre-built binaries
 		version = "v0.*",
-		-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
 
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
+			completion = {
+				menu = {
+					auto_show = function(ctx)
+						return ctx.mode ~= "cmdline"
+					end,
+				},
+				ghost_text = {
+					enabled = true,
+				},
+			},
+
 			keymap = {
 				preset = "default",
 				["<CR>"] = { "accept", "fallback" },
