11b4923fcb3efe8fc4339f0fc0f6984900a14c2c
Rickard Natt och Dag <rickard@nattochdag.dev>
2025-09-27 09:54:32 +0200

    chore: test mini.completion
    Needs winborder on pum to be nice. Wait for PR.

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -8,9 +8,40 @@ return {
 		"nvim-mini/mini.nvim",
 		version = false,
 		config = function()
+			-- Use this when pum supports winborder?
+			-- https://github.com/neovim/neovim/pull/25541
+			--
+			-- require("mini.completion").setup({
+			-- 	window = {
+			-- 		info = { height = 25, width = 80, border = "rounded" },
+			-- 		signature = { height = 25, width = 80, border = "rounded" },
+			-- 	},
+			-- })
+			--
+			-- -- Navigate with tab/s-tab
+			-- local imap_expr = function(lhs, rhs)
+			-- 	vim.keymap.set("i", lhs, rhs, { expr = true })
+			-- end
+			--
+			-- -- Add <CR> for selection
+			-- _G.cr_action = function()
+			-- 	-- If there is selected item in popup, accept it with <C-y>
+			-- 	if vim.fn.complete_info()["selected"] ~= -1 then
+			-- 		return "\25"
+			-- 	end
+			-- 	-- Fall back to plain `<CR>`. You might want to customize according
+			-- 	-- to other plugins. For example if 'mini.pairs' is set up, replace
+			-- 	-- next line with `return MiniPairs.cr()`
+			-- 	return "\r"
+			-- end
+			--
+			-- vim.keymap.set("i", "<CR>", "v:lua.cr_action()", { expr = true })
+			-- imap_expr("<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+			-- imap_expr("<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+
+			require("mini.extra").setup()
 			require("mini.pairs").setup()
 			require("mini.pick").setup()
-			require("mini.extra").setup()
 		end,
 	},
 
